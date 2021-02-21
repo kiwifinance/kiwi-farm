541234ab63bd1387f98c7e86bebcb9fabf221ff9
SolProc <info@cheeseswap.app>
2021-02-21 11:41:43 +0600

    correct sidenav

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -12,11 +12,11 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: 'https://cheeseswap.app',
+        href: 'https://cheeseswap.app/#/swap?inputCurrency=0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c&outputCurrency=0xcfdf8a80fecaeecc144fa74c0df8691bfd0e26e3',
       },
       {
         label: 'Liquidity',
-        href: 'https://cheeseswap.app/#/pool',
+        href: 'https://cheeseswap.app/#/add/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c/0xcfdf8a80fecaeecc144fa74c0df8691bfd0e26e3',
       },
     ],
   },
@@ -40,11 +40,11 @@ const config: MenuEntry[] = [
       },
       {
         label: 'Tokens',
-        href: 'https://info.cheeseswap.app/tokens',
+        href: 'https://info.cheeseswap.app/token/0xcfdf8a80fecaeecc144fa74c0df8691bfd0e26e3',
       },
       {
         label: 'Pairs',
-        href: 'https://info.cheeseswap.app/pairs',
+        href: 'https://info.cheeseswap.app/pair/0xdf8481f4ffb688d940f59f839c2613cc6c40a79e',
       },
       {
         label: 'Accounts',
